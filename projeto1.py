786c39f884cc0b5ba4b891f3fff407b3735f57f2
Alessandro-cesf <alessandro.amancio@escola.pr.gov.br>
2024-03-27 10:47:41 -0300

    Projeto1

---
+++ projeto1.py
@@ -0,0 +1,11 @@
+import os
+
+
+#print ("Ola mundo cruel")
+print("O 3DS é a turma numero 1 do sagrada")
+turma="2DS"
+pos=1
+print("o",turma,"e a turma numero",pos,"do sagrada")
+print(type(turma))
+print(type(pos))
+os.system("cls")
